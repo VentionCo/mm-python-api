2b839315e960efea445a2a8011e6941035cd8075
jack-dundas <jack.dundas@vention.cc>
2019-10-23 15:55:32 -0400

    started interactive absolute move

--- examples/userInput.py
+++ examples/userInput.py
@@ -1,7 +1,11 @@
 import sys
 import collections
 
-# Define a function to obtain user input
+#
+# Handles user interaction and logic for asking users yes/no or multiple choice questions.
+# @param question --- Description: A question for the user in string format
+# @param valid --- Description: A set of valid answers to the question (such as {y,n} or {true, false} or {a,b,c,d})
+#
 def user_input(question, valid):
     choice = ""
     while True:
@@ -19,7 +23,11 @@ def user_input(question, valid):
         else:
             print("Please respond with [" + " or ".join(valid.keys()) + "] \n")
 
-
+#
+# Prompts the user to check and confirm that sensor sensor xA and sensor xB are installed, where x represents a subset of axes 1,2,3
+# @param axis --- Description: represent which axes need to be checked. It can be a single axis or a set of 2 or 3 axes.
+# @return output --- Description: A dictionary whose keys are each sensor that needs verification (ex - 1A, 1B, 2A ...) and who's value is a boolean representing whether its been installed
+#
 def check_both_end_stops(axes):
     valid = {"y":True,"n":False}
     validAxes = {1,2,3}
