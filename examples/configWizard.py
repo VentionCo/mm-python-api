0bf5d6e7173d8cbaf075d95abc25c4b0903d26a0
jack-dundas <jack.dundas@vention.cc>
2019-10-25 11:32:44 -0400

    Improved CLI UI

--- examples/configWizard.py
+++ examples/configWizard.py
@@ -1,67 +1,99 @@
 import sys
 import collections
 
-#
-# Handles user interaction and logic for asking users yes/no or multiple choice questions.
-# @param question --- Description: A question for the user in string format
-# @param valid --- Description: A set of valid answers to the question (such as {y,n} or {true, false} or {a,b,c,d})
-#
-def user_input(question, valid):
-    choice = ""
-    exitCommands = ["Q","q","exit","Exit"]
+class configWizard:
 
-    while True:
-        print(question + " [" + " / ".join(valid.keys()) + "]")
-        if sys.version_info[0] == 2:
-            choice = raw_input().lower()
-        elif sys.version_info[0] == 3:
-            choice = input().lower()
-        else:
-            print("Application Error: Could not detect which python version is running")
-            return "Error"
+    validYN = {"Y":True, "N":False}
+    exitCommands = ["q"]
+    class userQuit(Exception): pass
 
-        if choice in valid:
-            return valid[choice]
-        elif choice in exitCommands:
-            raise Exception("Wizard Exited Prematurely")
-        else:
-            print("Please respond with [" + " or ".join(valid.keys()) + "] \n")
+    def __init__(self):
+        print("\n>>>\tMachine Motion Wizard Started - Press Q to quit at anytime")
+        print(">>>\t----------------------------------------------------------\n>>>")
 
-#
-# Prompts the user to check and confirm that sensor sensor xA and sensor xB are installed, where x represents a subset of axes 1,2,3
-# @param axis --- Description: represent which axes need to be checked. It can be a single axis or a set of 2 or 3 axes.
-# @return output --- Description: A dictionary whose keys are each sensor that needs verification (ex - 1A, 1B, 2A ...) and who's value is a boolean representing whether its been installed
-#
-def check_both_end_stops(axes):
-    valid = {"y":True,"n":False}
-    validAxes = {1,2,3}
-    output = dict()
+    def write(self, msg):
+        print(">>>\t" + msg , end = "\n>>>\t")
+        
+    def quit(self):
+        print(">>>\n>>>\tApplication Quit")
+        raise self.userQuit
+    #
+    # Handles user interaction and logic for asking users yes/no or multiple choice questions.
+    # @param question --- Description: A question for the user in string format
+    # @param valid --- Description: A set of valid answers to the question (such as {y,n} or {true, false} or {a,b,c,d})
+    #
+    def user_input(self, question, valid):
+        choice = ""
 
-    try:
-        axes = set(axes)
-    except TypeError:
-        if isinstance(axes, (int,long)):
-            axes = { axes }
-        else:
-            raise TypeError
+        # Starts loop that exits when user either quits or enters a valid choice
+        try:
+            while True:
+                self.write(question + " [" + " / ".join(valid.keys()) + "]")
+                if sys.version_info[0] == 2:
+                    choice = raw_input().lower()
+                elif sys.version_info[0] == 3:
+                    choice = input().lower()
+                else:
+                    self.write("Application Error: Could not detect which python version is running")
+                    return "Error"
 
-    if not axes.issubset(validAxes):
-        print("Application Error: Function expects a list of axes that must be checked")
+                if choice in valid:
+                    return valid[choice]
+                elif choice in self.exitCommands:
+                    self.quit()
+                else:
+                    self.write("Please respond with [" + " or ".join(valid.keys()) + "] \n")
+        except self.userQuit:
+            pass
 
-    for axis in axes:
-        question = "Application Message: Is Sensor " + str(axis) + "A plugged in?"
-        output[str(axis) + "A"] = user_input(question, valid)
-        question = "Application Message: Is Sensor " + str(axis) + "B plugged in?"
-        output[str(axis) + "B"] = user_input(question, valid)
+    #
+    # Prompts the user to check and confirm that sensor sensor xA and sensor xB are installed, where x represents a subset of axes 1,2,3
+    # @param axis --- Description: represent which axes need to be checked. It can be a single axis or a set of 2 or 3 axes.
+    # @return output --- Description: A dictionary whose keys are each sensor that needs verification (ex - 1A, 1B, 2A ...) and who's value is a boolean representing whether its been installed
+    #
+    def check_both_end_stops(self, axes):
+        valid = {"y":True,"n":False}
+        validAxes = {1,2,3}
+        output = dict()
 
-    return output
+        try:
+            axes = set(axes)
+        except TypeError:
+            if isinstance(axes, (int,long)):
+                axes = { axes }
+            else:
+                raise TypeError
+
+        if not axes.issubset(validAxes):
+            print("Application Error: Function expects a list of axes that must be checked")
+
+        for axis in axes:
+            question = "Application Message: Is Sensor " + str(axis) + "A plugged in?"
+            output[str(axis) + "A"] = user_input(question, valid)
+            question = "Application Message: Is Sensor " + str(axis) + "B plugged in?"
+            output[str(axis) + "B"] = user_input(question, valid)
+
+        return output
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Demo of configWizard that is executed when module is run as a script
 if __name__ == "__main__":
-    print("Launching wizard demo quesions. Press q to quit")
+    import configWizard
+    cw = configWizard.configWizard()
     question = "Do you like green eggs and ham?"
     valid = {"y":"I Do! I like them, Sam-I-Am!", "n":"I do not like them, Sam I am"}
-    response = user_input(question, valid)
+    response = cw.user_input(question, valid)
     print(response)
         
---
+++ examples/example--emitAbsoluteMoveInteractive.py
@@ -0,0 +1,54 @@
+##################################################
+## Absolute Move Interactive
+##################################################
+## Author: Francois Giguere
+## Version: 1.6.8
+## Email: info@vention.cc
+## Status: tested
+##################################################
+
+from _MachineMotion_1_6_8 import *
+import configWizard
+
+# Define a callback to process controller gCode responses if desired. This is mostly used for debugging purposes.
+def debug(data):
+    pass
+
+print ("Application Message: MachineMotion Program Starting \n")
+
+mm = MachineMotion(debug, DEFAULT_IP_ADDRESS.usb_windows)
+print ("Application Message: MachineMotion Controller Connected \n")
+
+# Configure the axis number 1, 8 uSteps and 150 mm / turn for a timing belt
+mm.configAxis(1, MICRO_STEPS.ustep_8, MECH_GAIN.timing_belt_150mm_turn)
+print ("Application Message: MachineMotion Axis 1 Configured \n")
+
+
+
+
+# Configuring the travel speed to 10000 mm / min
+mm.emitSpeed(10000)
+print ("Application Message: Speed configured \n")
+
+# Configuring the travel speed to 1000 mm / second^2
+mm.emitAcceleration(1000)
+print ("Application Message: Acceleration configured \n")
+
+# Homing axis 1
+mm.emitHome(1)
+print ("Application Message: Axis 1 going home \n")
+mm.waitForMotionCompletion()
+print ("Application Message: Axis 1 is at home \n")
+
+# Move the axis 1 to position 100 mm
+mm.emitAbsoluteMove(1, 100)
+print ("Application Message: Motion on-going ... \n")
+
+mm.waitForMotionCompletion()
+print ("Application Message: Motion completed \n")
+
+
+
+print ("Application Message: Program terminating ... \n")
+time.sleep(1)
+sys.exit(0)
