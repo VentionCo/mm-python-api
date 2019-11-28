2fa7d6e84b29cf7128463857a0b168f76015acf6
jack-dundas <jack.dundas@vention.cc>
2019-11-28 18:49:58 -0500

    getting ready for messy merge back into V2.2

--- examples/setPosition.py
+++ examples/setPosition.py
@@ -13,13 +13,12 @@ acceleration = 500                          #The constant acceleration and decce
 position = 100                              #The absolute position you'd like to move to
 mechGain = MECH_GAIN.rack_pinion_mm_turn    #The mechanical gain of the actuator on the axis
 
+mm.emitSpeed(speed)
+mm.emitAcceleration(acceleration)
 mm.configAxis(axis, MICRO_STEPS.ustep_16, mechGain)
 
 mm.emitHome(axis)
 print("Axis " + str(axis) + " homed")
-
-mm.emitSpeed(speed)
-mm.emitAcceleration(acceleration)
 
 print("Absolute Moves are referenced from from home")
 mm.emitAbsoluteMove(axis, position)
@@ -28,6 +27,7 @@ print("Axis " + str(axis) + " is " + str(position) + "mm away from home")
 
 mm.setPosition(axis, 0)
 print("Absolute moves on axis " + str(axis) + " are now referenced from " +  str(position) + "mm from home. ")
+
 position2 = 10
 mm.emitAbsoluteMove(axis, position)
 print("Axis " + str(axis) + " is now " + str(position2) + "mm from reference position and " + str(position + position2) + "mm from home")
