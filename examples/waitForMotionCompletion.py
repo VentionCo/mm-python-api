5b899adb0e0cc84c14a2504d032cec101c17a168
guirb <guillaume.robitaille@vention.cc>
2020-01-07 15:03:06 -0500

    Boolean should not be strings

--- examples/waitForMotionCompletion.py
+++ examples/waitForMotionCompletion.py
@@ -16,7 +16,7 @@ machine_motion_example = MachineMotion(templateCallback, DEFAULT_IP_ADDRESS.usb_
 # Homing axis one
 machine_motion_example.emitHome(1)
 # Wait for the message to be acknowledged by the motion controller
-while machine_motion_example.isReady() != "true": pass
+while machine_motion_example.isReady() != True: pass
 machine_motion_example.waitForMotionCompletion()
 
 print ( "--> This line executes after the motion controller has acknowledged the reception of the command." )
