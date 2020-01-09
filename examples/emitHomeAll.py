6dbcc0434940307a0efb36b283897e2e0778d49a
jack-dundas <jack.dundas@vention.cc>
2020-01-09 18:30:25 -0500

    updated import statements

--- examples/emitHomeAll.py
+++ examples/emitHomeAll.py
@@ -1,7 +1,5 @@
-import sys, os
-this_script_folder = os.path.dirname(__file__)
-relative_path_to_MachineMotion_folder = os.path.dirname("../")
-sys.path.insert(1, os.path.join(this_script_folder,relative_path_to_MachineMotion_folder))
+import sys
+sys.path.append("..")
 from MachineMotion import *
 
 mm = MachineMotion(DEFAULT_IP_ADDRESS.usb_windows)
