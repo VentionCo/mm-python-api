3e083c39e787fa3f831c48d54b63c5c1a6c48fa8
francois-giguere <francois.giguere@vention.cc>
2020-01-12 14:14:12 -0500

    schunk example api

--- examples/schunk.py
+++ examples/schunk.py
@@ -3,13 +3,6 @@ sys.path.append("..")
 from _MachineMotion import *
 
 mm = MachineMotion(DEFAULT_IP_ADDRESS.usb_windows)
-axis = AXIS_NUMBER.DRIVE1
-
-mm.configAxis(axis, MICRO_STEPS.ustep_8, MECH_GAIN.timing_belt_150mm_turn)
-mm.emitHome(axis)
-print ("Application Message: Axis "+ str(axis) +" is going home")
-mm.waitForMotionCompletion()
-print("Application Message: Axis "+ str(axis) +" is at home")
 
 schunkGripper = SchunkGripper(1, mm)
 
@@ -18,10 +11,3 @@ for i in range (0, 100):
     time.sleep(1)
     schunkGripper.close()
     time.sleep(1)
-
-time.sleep(5)
-
-
-
-
-
