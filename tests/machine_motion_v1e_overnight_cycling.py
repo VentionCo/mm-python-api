632374054bd9a945df3d2b8d305a374fa269144b
guirb <guillaume.robitaille@vention.cc>
2020-01-13 08:53:32 -0500

    Added eStop release and resetsystem before running program

--- tests/machine_motion_v1e_overnight_cycling.py
+++ tests/machine_motion_v1e_overnight_cycling.py
@@ -13,6 +13,9 @@ def templateCallback(data):
 
 mm = MachineMotion(templateCallback, DEFAULT_IP_ADDRESS.usb_windows)
 
+mm.releaseEstop()
+mm.resetSystem()
+
 mm.configAxis(1, MICRO_STEPS.ustep_8, MECH_GAIN.timing_belt_150mm_turn)
 mm.configAxis(2, MICRO_STEPS.ustep_8, MECH_GAIN.timing_belt_150mm_turn)
 mm.configAxis(3, MICRO_STEPS.ustep_8, MECH_GAIN.timing_belt_150mm_turn)
