5590110c8614639717d87328e1d129e501c1df54
guirb <guillaume.robitaille@vention.cc>
2020-01-21 09:59:39 -0500

    Improving test with axis as parameter

--- tests/conveyor_mix.py
+++ tests/conveyor_mix.py
@@ -17,30 +17,30 @@ while (True) :
     cacceleration = 20000
     cduration = 3.0
     stoptime = 2.0
+    axis = "Y"
 
     print("Conveyor +")
-    m1.emitgCode("V4 S0 A1000000 X")
+    m1.emitgCode("V4 S0 A1000000 " + axis)
     time.sleep(stoptime)
-    m1.emitgCode("V5 X2")
-    m1.emitgCode("V4 S%d A%d X" % (cspeed, cacceleration))
+    m1.emitgCode("V5 " + axis + "2")
+    m1.emitgCode(("V4 S%d A%d " + axis) % (cspeed, cacceleration))
     time.sleep(cduration)
     print("Position -")
-    m1.emitgCode("V5 X1")
+    m1.emitgCode("V5 " + axis + "1")
     m1.emitgCode("G91")
-    m1.emitgCode("G0 X-300")
+    m1.emitgCode("G0 " + axis + "-300")
     m1.waitForMotionCompletion()
     time.sleep(0.1)
 
     print("Conveyor -")
-    m1.emitgCode("V4 S0 A1000000 X")
+    m1.emitgCode("V4 S0 A1000000 " + axis)
     time.sleep(stoptime)
-    m1.emitgCode("V5 X2")
-    m1.emitgCode("V4 S-%d A%s X" % (cspeed, cacceleration))
+    m1.emitgCode("V5 " + axis + "2")
+    m1.emitgCode(("V4 S-%d A%s " + axis) % (cspeed, cacceleration))
     time.sleep(cduration)
     print("Position +")
-    m1.emitgCode("V5 X1")
+    m1.emitgCode("V5 " + axis + "1")
     m1.emitgCode("G91")
-    m1.emitgCode("G0 X300")
+    m1.emitgCode("G0 " + axis + "300")
     m1.waitForMotionCompletion()
     time.sleep(0.1)
-
